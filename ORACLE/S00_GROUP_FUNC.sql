0aaa9ff99837e8bcad9a6ba0282fd2eaf90ec3c9
woogieReal <75457703+woogieReal@users.noreply.github.com>
2021-02-04 16:49:03 +0900

    2021-02-04

--- ORACLE/S00_GROUP_FUNC.sql
+++ ORACLE/S00_GROUP_FUNC.sql
@@ -392,7 +392,334 @@ ORDER BY deptno, job;
 --           SALESMAN                 1400          4
 --                                  2077.1         12
 
+--1.CTAS
+--CREATE TABLE unpivot
+--AS
+--SELECT * FROM(SELECT deptno,job,empno FROM emp)
+--PIVOT(
+--	COUNT(empno) FOR job IN(
+--		 'CLERK'     AS "CLERK"
+--		,'SALESMAN'  AS "SALESMAN"
+--		,'PRESIDENT' AS "PRESIDENT"
+--		,'MANAGER'   AS "MANAGER"
+--		,'ANALYST'   AS "ANALYST"
+--	)
+--)
+--ORDER BY deptno;
 
+--2.생성된 TABLE 데이터 확인
+SELECT *
+FROM unpivot;
+--   DEPTNO      CLERK   SALESMAN  PRESIDENT    MANAGER    ANALYST
+----------- ---------- ---------- ---------- ---------- ----------
+--       10          1          0          1          1          0
+--       20          1          0          0          1          1
+--       30          1          4          0          1          0
+
+--3. UNPIVOT
+--PIVOT된 결과를 되돌리는 기능이 아닌 합쳐있는 것을 단지 풀어서 보여주는 역할
+SELECT * FROM unpivot
+UNPIVOT(
+	empno FOR job IN(
+		CLERK,SALESMAN,PRESIDENT,MANAGER,ANALYST
+	)
+);
+--    DEPTNO JOB                     EMPNO
+------------ ------------------ ----------
+--        10 CLERK                       1
+--        10 SALESMAN                    0
+--        10 PRESIDENT                   1
+--        10 MANAGER                     1
+--        10 ANALYST                     0
+--        20 CLERK                       1
+--        20 SALESMAN                    0
+--        20 PRESIDENT                   0
+--        20 MANAGER                     1
+--        20 ANALYST                     1
+--        30 CLERK                       1
+--        30 SALESMAN                    4
+--        30 PRESIDENT                   0
+--        30 MANAGER                     1
+--        30 ANALYST                     0
+
+--========================================================
+--LAG: 이전 행 값을 가져올 때 사용
+--LAG(출력할 컬럼명,OFFSET,기본출력값)
+--OVER(Query_partition구문,ORDER BY 정렬할 컬럼)
+SELECT ename
+	,hiredate
+	,sal
+	,LAG(sal,1,0) OVER(ORDER BY hiredate)"LAG"
+FROM emp;
+--ENAME                HIREDATE        SAL        LAG
+---------------------- -------- ---------- ----------
+--SMITH                80/12/17        800          0
+--ALLEN                81/02/20       1600        800
+--WARD                 81/02/22       1250       1600
+--JONES                81/04/02       2975       1250
+--BLAKE                81/05/01       2850       2975
+--CLARK                81/06/09       2450       2850
+--TURNER               81/09/08       1500       2450
+--MARTIN               81/09/28       1250       1500
+--KING                 81/11/17       5000       1250
+--JAMES                81/12/03        950       5000
+--FORD                 81/12/03       3000        950
+--MILLER               82/01/23       1300       3000
+
+--OFFSET은 3번째 전 것으로 설정
+--기본값은 999
+SELECT ename
+	,hiredate
+	,sal
+	,LAG(sal,3,999) OVER(ORDER BY hiredate)"LAG"
+FROM emp;
+--ENAME                HIREDATE        SAL        LAG
+---------------------- -------- ---------- ----------
+--SMITH                80/12/17        800        999
+--ALLEN                81/02/20       1600        999
+--WARD                 81/02/22       1250        999
+--JONES                81/04/02       2975        800
+--BLAKE                81/05/01       2850       1600
+--CLARK                81/06/09       2450       1250
+--TURNER               81/09/08       1500       2975
+--MARTIN               81/09/28       1250       2850
+--KING                 81/11/17       5000       2450
+--JAMES                81/12/03        950       1500
+--FORD                 81/12/03       3000       1250
+--MILLER               82/01/23       1300       5000
+
+--LEAD: 이후 행의 값을 가져옴
+--LEAD(출력할 컬럼명,OFFSET,기본출력값)
+--OVER(Query_partition구문,ORDER BY 정렬할 컬럼)
+SELECT ename
+	,hiredate
+	,sal
+	,LEAD(sal,1,0) OVER(ORDER BY hiredate)"LEAD"
+FROM emp;
+--ENAME                HIREDATE        SAL        LEAD
+---------------------- -------- ---------- ----------
+--SMITH                80/12/17        800       1600
+--ALLEN                81/02/20       1600       1250
+--WARD                 81/02/22       1250       2975
+--JONES                81/04/02       2975       2850
+--BLAKE                81/05/01       2850       2450
+--CLARK                81/06/09       2450       1500
+--TURNER               81/09/08       1500       1250
+--MARTIN               81/09/28       1250       5000
+--KING                 81/11/17       5000        950
+--JAMES                81/12/03        950       3000
+--FORD                 81/12/03       3000       1300
+--MILLER               82/01/23       1300          0
+
+--OFFSET은 4번째 이후로 설정
+--기본값은 5609
+SELECT ename
+	,hiredate
+	,sal
+	,LEAD(sal,4,5609) OVER(ORDER BY hiredate)"LEAD"
+FROM emp;
+--ENAME                HIREDATE        SAL       LEAD
+---------------------- -------- ---------- ----------
+--SMITH                80/12/17        800       2850
+--ALLEN                81/02/20       1600       2450
+--WARD                 81/02/22       1250       1500
+--JONES                81/04/02       2975       1250
+--BLAKE                81/05/01       2850       5000
+--CLARK                81/06/09       2450        950
+--TURNER               81/09/08       1500       3000
+--MARTIN               81/09/28       1250       1300
+--KING                 81/11/17       5000       5609
+--JAMES                81/12/03        950       5609
+--FORD                 81/12/03       3000       5609
+--MILLER               82/01/23       1300       5609
+
+--RANK: 특정데이터의 순위를 출력
+--RANK(조건값) WITHIN GROUP (ORDER BY 조건값 컬럼명 [ASC | DESC] )
+SELECT RANK('SMITH') WITHIN GROUP (ORDER BY ename)"RANK"
+FROM emp
+ORDER BY ename;
+--      RANK
+------------
+--        10
+
+--RANK_OVER: 전체데이터의 순위를 출력
+--RANK() OVER (ORDER BY sal)
+--ex.2위 3개 뒤에는 5위
+SELECT empno,ename,sal
+	,RANK() OVER(ORDER BY sal DESC)"RANK OVER"
+FROM emp;
+--     EMPNO ENAME                       SAL  RANK OVER
+------------ -------------------- ---------- ----------
+--      7839 KING                       5000          1
+--      7902 FORD                       3000          2
+--      7566 JONES                      2975          3
+--      7698 BLAKE                      2850          4
+--      7782 CLARK                      2450          5
+--      7499 ALLEN                      1600          6
+--      7844 TURNER                     1500          7
+--      7934 MILLER                     1300          8
+--      7654 MARTIN                     1250          9
+--      7521 WARD                       1250          9
+--      7900 JAMES                       950         11
+--      7369 SMITH                       800         12
+
+--특정부서의 월급순위를 출력
+SELECT empno,ename,sal
+	,RANK() OVER(ORDER BY sal DESC)"RANK OVER"
+FROM emp
+WHERE deptno = 10;
+--     EMPNO ENAME                       SAL  RANK OVER
+------------ -------------------- ---------- ----------
+--      7839 KING                       5000          1
+--      7782 CLARK                      2450          2
+--      7934 MILLER                     1300          3
+
+--모든 부서별로 월급순위를 출력
+--PARTITION BY 사용
+SELECT deptno,empno,ename,sal
+	,RANK() OVER(PARTITION BY deptno ORDER BY sal DESC)"RANK OVER"
+FROM emp;
+--    DEPTNO      EMPNO ENAME                       SAL  RANK OVER
+------------ ---------- -------------------- ---------- ----------
+--        10       7839 KING                       5000          1
+--        10       7782 CLARK                      2450          2
+--        10       7934 MILLER                     1300          3
+--        20       7902 FORD                       3000          1
+--        20       7566 JONES                      2975          2
+--        20       7369 SMITH                       800          3
+--        30       7698 BLAKE                      2850          1
+--        30       7499 ALLEN                      1600          2
+--        30       7844 TURNER                     1500          3
+--        30       7521 WARD                       1250          4
+--        30       7654 MARTIN                     1250          4
+--        30       7900 JAMES                       950          6
+
+--DENSE_RANK
+--순위 출력함수(ex.2위 3개 뒤에는 3위)
+SELECT empno,ename,sal
+	,DENSE_RANK() OVER(ORDER BY sal DESC)"DENSE_RANK"
+FROM emp;
+--     EMPNO ENAME                       SAL DENSE_RANK
+------------ -------------------- ---------- ----------
+--      7839 KING                       5000          1
+--      7902 FORD                       3000          2
+--      7566 JONES                      2975          3
+--      7698 BLAKE                      2850          4
+--      7782 CLARK                      2450          5
+--      7499 ALLEN                      1600          6
+--      7844 TURNER                     1500          7
+--      7934 MILLER                     1300          8
+--      7654 MARTIN                     1250          9
+--      7521 WARD                       1250          9
+--      7900 JAMES                       950         10
+--      7369 SMITH                       800         11
+
+--ROW_NUMBER: 순위출력
+--RANK와 DENSE_RANK와 다르게 동일순위 없이 같은 값이라도 다른 순위를 부여
+--동일 값이면 ROWID 순으로 순위부여(P405참고)
+SELECT empno,ename,sal,ROWID
+	,RANK() OVER (ORDER BY sal DESC)"RANK"
+	,DENSE_RANK() OVER(ORDER BY sal DESC)"DENSE_RANK"
+	,ROW_NUMBER() OVER(ORDER BY sal DESC)"ROW_NUMBER"
+FROM emp;
+-- EMPNO ENAME        SAL ROWID                 RANK DENSE_RANK ROW_NUMBER
+-------- -------- ------- ------------------ ------- ---------- ----------
+--  7839 KING        5000 AAAE6LAAEAAAAK+AAH       1          1          1
+--  7902 FORD        3000 AAAE6LAAEAAAAK+AAK       2          2          2
+--  7566 JONES       2975 AAAE6LAAEAAAAK+AAD       3          3          3
+--  7698 BLAKE       2850 AAAE6LAAEAAAAK+AAF       4          4          4
+--  7782 CLARK       2450 AAAE6LAAEAAAAK+AAG       5          5          5
+--  7499 ALLEN       1600 AAAE6LAAEAAAAK+AAB       6          6          6
+--  7844 TURNER      1500 AAAE6LAAEAAAAK+AAI       7          7          7
+--  7934 MILLER      1300 AAAE6LAAEAAAAK+AAL       8          8          8
+--  7654 MARTIN      1250 AAAE6LAAEAAAAK+AAE       9          9          9
+--  7521 WARD        1250 AAAE6LAAEAAAAK+AAC       9          9         10
+--  7900 JAMES        950 AAAE6LAAEAAAAK+AAJ      11         10         11
+--  7369 SMITH        800 AAAE6LAAEAAAAK+AAA      12         11         12
+
+--SUM() OVER: 누계 구하기
+SELECT *
+FROM panmae;
+--P_DATE               P_CODE      P_QTY    P_TOTAL P_STORE
+------------------ ---------- ---------- ---------- ----------
+--20110101                100          3       2400 1000
+--20110101                101          5       4500 1001
+--20110101                102          2       2000 1003
+--20110101                103          6       5400 1004
+--20110102                102          2       2000 1000
+--20110102                103          5       4500 1002
+--20110102                104          3       2400 1002
+--20110102                105          2       3000 1000
+--20110103                100         10       8000 1004
+--20110103                100          2       1600 1000
+--20110103                100          3       2400 1001
+--20110103                101          4       3600 1003
+--20110104                100          2       1600 1002
+--20110104                100          4       3200 1003
+--20110104                100          5       4000 1004
+--20110104                101          3       2700 1001
+--20110104                101          4       3600 1002
+--20110104                101          3       2700 1003
+--20110104                102          4       4000 1001
+--20110104                102          2       2000 1002
+--20110104                103          2       1800 1003
+
+SELECT P_DATE, P_CODE, P_QTY, P_TOTAL
+	,SUM(P_TOTAL) OVER(PARTITION BY P_CODE ORDER BY P_TOTAL)"SUM()_OVER"
+FROM panmae
+WHERE P_STORE = 1000;
+--P_DATE               P_CODE      P_QTY    P_TOTAL SUM()_OVER
+------------------ ---------- ---------- ---------- ----------
+--20110103                100          2       1600       1600
+--20110101                100          3       2400       4000
+--20110102                102          2       2000       2000
+--20110102                105          2       3000       3000
+
+SELECT P_DATE, P_STORE, P_CODE, P_QTY, P_TOTAL
+	,SUM(P_TOTAL) OVER(PARTITION BY P_CODE,P_STORE ORDER BY P_TOTAL)"SUM()_OVER"
+FROM panmae;
+--P_DATE           P_STORE        P_CODE      P_QTY    P_TOTAL SUM()_OVER
+------------------ ---------- ---------- ---------- ---------- ----------
+--20110103         1000              100          2       1600       1600
+--20110101         1000              100          3       2400       4000
+--20110103         1001              100          3       2400       2400
+--20110104         1002              100          2       1600       1600
+--20110104         1003              100          4       3200       3200
+--20110104         1004              100          5       4000       4000
+--20110103         1004              100         10       8000      12000
+--20110104         1001              101          3       2700       2700
+--20110101         1001              101          5       4500       7200
+--20110104         1002              101          4       3600       3600
+--20110104         1003              101          3       2700       2700
+--20110103         1003              101          4       3600       6300
+--20110102         1000              102          2       2000       2000
+--20110104         1001              102          4       4000       4000
+--20110104         1002              102          2       2000       2000
+--20110101         1003              102          2       2000       2000
+--20110102         1002              103          5       4500       4500
+--20110104         1003              103          2       1800       1800
+--20110101         1004              103          6       5400       5400
+--20110102         1002              104          3       2400       2400
+--20110102         1000              105          2       3000       3000
+
+--RATIO_TO_REPORT: 비율을 구하는 함수
+--panmae 테이블에서 100번 제품의 판매내역과 각 판매점별 판매비중
+SELECT P_STORE, P_TOTAL, P_QTY
+	,SUM(P_QTY) OVER()"T_QTY"
+	,SUM(P_TOTAL) OVER()"P_TOTAL"
+	,ROUND(RATIO_TO_REPORT(SUM(P_QTY)) OVER()*100) "RATION_QTY"
+FROM panmae
+WHERE P_CODE = 100
+GROUP BY P_STORE, P_TOTAL, P_QTY; 
+--P_STORE       P_TOTAL      P_QTY      T_QTY    P_TOTAL RATION_QTY
+------------ ---------- ---------- ---------- ---------- ----------
+--1004             8000         10         29      23200         34
+--1000             1600          2         29      23200          7
+--1000             2400          3         29      23200         10
+--1002             1600          2         29      23200          7
+--1003             3200          4         29      23200         14
+--1004             4000          5         29      23200         17
+--1001             2400          3         29      23200         10
 
 
 
