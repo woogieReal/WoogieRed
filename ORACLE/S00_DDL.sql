f186dd7ab897c3f238350d120a30d1c61c74eb99
woogieReal <75457703+woogieReal@users.noreply.github.com>
2021-02-09 21:18:29 +0900

    2021-02-09(3)
    http://www.tcpschool.com/css/css_position_float
    http://www.tcpschool.com/css/css_position_align
    1. TCP CSS 22
      - float
      - clear
      - overflow
    2. TCP CSS 23
      - align (margin, position, float)

--- ORACLE/S00_DDL.sql
+++ ORACLE/S00_DDL.sql
@@ -125,6 +125,9 @@ DROP COLUMN LOC
 ALTER TABLE dept6
 DROP COLUMN LOC CASCADE CONSTRAINTS;
 
+--TRUNCATE
+--테이블의 데이터를 삭제하고 사용하고 있던 공간을 반납(DELETE는 데이터만 삭제함)
+TRUNCATE TABLE dept6;
 
 
 
--- ORACLE/truncate.sql
+++
@@ -1,4 +0,0 @@
---TRUNCATE
---테이블의 데이터를 삭제하고 사용하고 있던 공간을 반납(DELETE는 데이터만 삭제함)
---TRUNCATE TABLE dept6;
-
