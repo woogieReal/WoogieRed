deabc25f9260287b02e4366cc27f6715d0d90d6a
woogieReal <75457703+woogieReal@users.noreply.github.com>
2021-06-03 22:26:04 +0900

    리액트 정리 커밋2

--- REACT/react-app01/src/App.js
+++ REACT/react-app01/src/App.js
@@ -1,37 +1,79 @@
 import './App.css';
 import { Component } from 'react';
 import TOC from './components/TOC';
-import Content from './components/Content';
+import ReadContent from './components/ReadContent';
 import Subject from './components/Subject';
+import Control from './components/Control';
+import CreateContent from './components/CreateContent';
+import UpdateContent from './components/UpdateContent';
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      mode: 'read',
+      mode: 'welcome',
       selected_content_id: 1,
       welcome: {title: 'Welcome', desc: 'Hello, React!'},
       subject: {title: 'WEB', sub: 'World Wide Web!!'},
       contents: [
-        {id:1, title:'HTML', desc:'HTML is HyperText ...'},
-        {id:2, title:'CSS', desc:'CSS is for design ...'},
-        {id:3, title:'JAVASCRIPT', desc:'JAVASCRIPT is for interactive ...'}
+        {id:1, title:'HTML', desc:'HTML is HyperText.'},
+        {id:2, title:'CSS', desc:'CSS is for design.'},
+        {id:3, title:'JAVASCRIPT', desc:'JAVASCRIPT is for interactive.'}
       ]
     }
   }
 
-  render() {
-    let _title, _desc = null;
+  getReadContent() {
+    return this.state.contents.find(content => {
+      return content.id === this.state.selected_content_id;
+    });
+  }
+
+  getContent() {
+    let _title, _desc, _article = null;
     if(this.state.mode === 'welcome'){
       _title = this.state.welcome.title;
       _desc = this.state.welcome.desc;
+      _article = <ReadContent title={_title} desc={_desc}></ReadContent>
     } else if(this.state.mode === 'read'){
-      const findContent = this.state.contents.find(content => {
-        return content.id === this.state.selected_content_id;
-      });
+      const findContent = this.getReadContent();
       _title = findContent.title;
       _desc = findContent.desc;
+      _article = <ReadContent title={_title} desc={_desc}></ReadContent>
+    } else if(this.state.mode === 'create') {
+      const nextId = this.state.contents[this.state.contents.length-1].id + 1;
+
+      _article = <CreateContent onSubmit={function (_title, _desc){
+        let _contents = Array.from(this.state.contents);
+        _contents.push({id: nextId,title: _title, desc: _desc});
+
+        this.setState({
+          contents: _contents,
+          mode: 'read',
+          selected_content_id: nextId
+        });
+      }.bind(this)}></CreateContent>
+    } else if(this.state.mode === 'update') {
+      const findContent = this.getReadContent();
+      _article = <UpdateContent data={findContent} onSubmit={function (_id, _title, _desc){
+        let _contents = [];
+        this.state.contents.forEach(content => {
+          if(content.id === _id) content = {id: _id, title: _title, desc: _desc};
+          _contents.push(content);
+        });
+        this.setState({
+          contents: _contents,
+          mode: 'read'
+        });
+        
+      }.bind(this)}></UpdateContent>
     }
+    return _article;
+  }
+
+  render() {
+    console.log('App render');
+    
     return (
       <div className="App">
         <Subject 
@@ -51,7 +93,18 @@ class App extends Component {
             });
           }.bind(this)} data={this.state.contents}>
         </TOC>
-        <Content title={_title} desc={_desc}></Content>
+        <Control onChangeMode={function (_mode) {
+          if(_mode === 'delete'){
+            if(!window.confirm('really?')) return;
+            const _contents = this.state.contents.filter(content => {
+              return content.id !== this.state.selected_content_id;
+            });
+            this.setState({contents: _contents, mode: 'welcome'});
+          } else {
+            this.setState({mode: _mode});
+          }
+        }.bind(this)}></Control>
+        {this.getContent()}
       </div>
     );
   }
--- REACT/react-app01/src/components/Subject.js
+++ REACT/react-app01/src/components/Subject.js
@@ -1,18 +1,23 @@
 import { Component } from 'react';
 
 class Subject extends Component {
-    render() {
-      return (
-        <header>
-          <h1><a href="/" onClick={function(e){
-            e.preventDefault();
-            this.props.onChangePage();
-          }.bind(this)}>{this.props.title}</a></h1>
-          <hr></hr>
-          {this.props.sub}
-        </header>
-      );
-    }
+  shouldComponentUpdate(newProps){
+    if(this.props.data === newProps.data) return false
+    else return true;
   }
+  render() {
+    console.log('Subject render');
+    return (
+      <header>
+        <h1><a href="/" onClick={function(e){
+          e.preventDefault();
+          this.props.onChangePage();
+        }.bind(this)}>{this.props.title}</a></h1>
+        <hr></hr>
+        {this.props.sub}
+      </header>
+    );
+  }
+}
 
 export default Subject;
